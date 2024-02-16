3e80f38b1c84b1d8498b579ea815b22c92acbdef
kimjihoon1511 <kimjihooh1234@gmail.com>
2024-02-16 10:58:04 +0900

    js 이벤트 다루는 방법을 공부한 자료 입니다.

---
+++ 7_event-form/7_event-form.js
@@ -0,0 +1,31 @@
+let inputBox = document.querySelector('#input-box');
+let img = document.querySelector('img');
+/*inputBox.addEventListener('keypress', function (event) {
+  console.log('keypress');
+});
+
+inputBox.addEventListener('keydown', function (event) {
+  console.log('keydown');
+});*/
+
+/*inputBox.addEventListener('keyup', function (event) {
+  console.log('evnet.key:', event.key);
+  console.log('evnet.keyCode:', event.keyCode);
+});*/
+
+/*inputBox.addEventListener('focus', function (event) {
+  alert('focus');
+});*/
+
+/*inputBox.addEventListener('blur', function (event) {
+  alert('blur');
+});*/
+
+/*inputBox.addEventListener('change', function (event) {
+  console.log('change');
+});*/
+
+img.addEventListener('error', function (event) {
+  console.log('error');
+  event.target.src = '../../img/apple.jpg';
+});
